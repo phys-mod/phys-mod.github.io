0723c9ecdb8f2c3727081ec54b13e5c98f170dc8
delva <pacome.delva@obspm.fr>
2022-01-10 15:12:53 +0100

    first commit

---
+++ phys-mod/source/notebooks/generate-notebook.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for rep in ./0*; do
+	# create etu repertory
+	echo creating rep: $eturep
+	echo --------------
+	eturep=etu-`expr substr ${rep} 3 200`/
+	mkdir -p $eturep
+	# remove solution cells from notebooks
+	cd $rep
+	cp *.png *.jpg *.csv *.txt *.dat ../$eturep 2>/dev/null
+	for file in ./*.ipynb; do
+		etufile=etu-`expr substr ${file} 3 200`
+		echo convert $file to $etufile
+		jupyter nbconvert $file --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='{"solution"}' --to notebook --output $etufile
+		mv $etufile ../$eturep$etufile
+		echo
+	done
+	cd ..
+	echo
+done
+# zip etu repertories and delete
+zip -r calepins-etu.zip etu*
+rm -r etu*
